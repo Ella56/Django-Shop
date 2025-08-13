1166a1f3f25562e70a24c853cd4727520d6779ef
Ella56 <zamani56.elham@gmail.com>
2025-08-14 01:30:30 +0330

    index-factor

--- root/views.py
+++ root/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView
 from .forms import ContactForm
 from django.contrib import messages
+from product.models import Product, SpecialOffer
+from django.utils import timezone
+from blog.models import Blog
 
 # Create your views here.
 
@@ -9,6 +12,18 @@ from django.contrib import messages
 
 class HomeView(TemplateView):
     template_name = 'root/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['selected_products'] = Product.objects.all().order_by('-total_favorites')[:4]
+        context['discounted_products'] = Product.objects.filter(discount_price__gt=0).order_by('-discount_price')[:100]
+        context['popular_products'] = Product.objects.all().order_by('-total_views')[:12]
+        special_offers = SpecialOffer.objects.filter(start_date__lte=timezone.now(), end_date__gte=timezone.now()
+        ).order_by("-product__discount_price")
+        context['blogs'] = Blog.objects.filter(status=True).order_by('-created_at')[:3]
+        context['special_offers'] = special_offers
+        return context
+
 
 
 
